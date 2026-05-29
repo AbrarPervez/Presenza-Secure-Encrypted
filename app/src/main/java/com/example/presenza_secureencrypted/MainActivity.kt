f2398a7dc9b297fb3197bf85ca8e0b2fb6bd4dde
Abrar Pervez <abrarpervez24@gmail.com>
2026-05-29 21:06:37 +0300

    Still didnt fix but maybe close

--- app/src/main/java/com/example/presenza_secureencrypted/MainActivity.kt
+++ app/src/main/java/com/example/presenza_secureencrypted/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.presenza_secureencrypted
 
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 
@@ -9,22 +10,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "onCreate called")
+        try {
+            setContentView(R.layout.activity_main)
 
-        // 1. Start the Animated Gradient Background
-        val rootLayout = findViewById<ConstraintLayout>(R.id.main_root)
-        val animDrawable = rootLayout.background as? AnimationDrawable
-        animDrawable?.apply {
-            setEnterFadeDuration(2000)
-            setExitFadeDuration(4000)
-            start()
-        }
+            // 1. Start the Animated Gradient Background
+            val rootLayout = findViewById<ConstraintLayout>(R.id.main_root)
+            val animDrawable = rootLayout.background as? AnimationDrawable
+            animDrawable?.apply {
+                setEnterFadeDuration(2000)
+                setExitFadeDuration(4000)
+                start()
+            }
 
-        // Load Login Page by default
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, LoginFragment())
-                .commit()
+            // Load Login Page by default
+            if (savedInstanceState == null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, LoginFragment())
+                    .commit()
+            }
+        } catch (e: Exception) {
+            Log.e("MainActivity", "Error in onCreate", e)
+            throw e
         }
     }
 }
